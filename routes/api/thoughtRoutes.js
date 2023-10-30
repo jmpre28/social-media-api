8fef6d0119ff8100b0d5f29a6c12f2aa2f39b169
Jake Preciado <jacobpreciado@gmail.com>
2023-10-29 18:42:15 -0600

    condensed reaction routes into thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,11 +4,17 @@ const {
     getSingleThought,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    removeReaction
 } = require('../../controllers/thoughtController.js');
 
 router.route('/').get(getAllThoughts).post(createThought);
 
 router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:id/reactions').post(addReaction);
+
+router.route('/:id/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
